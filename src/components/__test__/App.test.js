8f26868ec53557d9b35069d4a1a0e3e29481307e
SVETO <svetostoj@hotmail.com>
2019-03-03 18:10:09 +1100

    beforeEach afterEach

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+beforeEach(()=>{
+     wrapped = shallow(<App/>)
+});
 
 it('shows a comment box', ()=> {
-    const div = document.createElement('div');
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-    ReactDOM.render(<App />, div);
-
-    // Look into the div and check if CommentBox is in there
-    console.log(div.innerHTML);
-    ReactDOM.unmountComponentAtNode(div);
-
+it ('shows a comment list', ()=> { 
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
