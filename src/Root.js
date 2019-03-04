20e35cccf8a01a9b264e570fddf64a7d4d1eb1d7
SVETO <svetostoj@hotmail.com>
2019-03-05 10:45:28 +1100

    Lessons 50 51 Redux init state cherio

--- src/Root.js
+++ src/Root.js
@@ -3,11 +3,19 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-
-export default props  => {
+export default ({children, initialState={}})  => {
     return (
-        <Provider store={createStore(reducers, {})}>
-            {props.children}
+        <Provider store={createStore(reducers, initialState)}>
+            {children}
         </Provider>
     );
 };  
+
+// RE-FACTORING
+// export default props  => {
+//     return (
+//         <Provider store={createStore(reducers, props.initialState)}>
+//             {props.children}
+//         </Provider>
+//     );
+// };  
