7e604dac7f835434c0ced7e2b68e0bedafca3f28
SVETO <svetostoj@hotmail.com>
2019-03-05 12:58:00 +1100

    Lessons 51 52 53 Fetch API Parsing comments list

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -20,6 +20,7 @@ class CommentBox extends Component {
 
     render() {
         return (
+            <div>
             <form onSubmit={this.handleSubmit}>
                 <h4>Add a Comment</h4>
                 <textarea onChange={this.handleChange} value={this.state.comment} />
@@ -27,6 +28,9 @@ class CommentBox extends Component {
                     <button>Submit Comment</button>
                 </div>
             </form>
+
+            <button onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
         );
     };
 };
--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -17,7 +17,7 @@ afterEach(()=> {
 
 it ('has a text area and submit button', ()=> {
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(2);
 });
 
 describe('the text area', ()=>{
